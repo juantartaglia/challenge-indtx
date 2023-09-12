49da0e0179aecfbb9b8dcc843a78e40860d6e3dc
Juan Manuel Tartaglia <juan.tartaglia@naranjax.com>
2023-09-12 10:04:57 +0200

    feat: init test layer

---
+++ src/test/java/com/jmt/challengeindtx/ChallengeIndtxApplicationTests.java
@@ -0,0 +1,14 @@
+package com.jmt.challengeindtx;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+
+class ChallengeIndtxApplicationTests {
+
+	@Test
+	@Disabled
+	void contextLoads() {
+	}
+
+}
